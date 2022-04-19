d60e86eccc33ebd0d5cec626efe0a8b985a71a93
sawirricardo <sawir.ricardo@gmail.com>
2022-04-19 11:16:39 +0700

    reformat test

--- tests/MidtransTest.php
+++ tests/MidtransTest.php
@@ -1,10 +1,11 @@
 <?php
 
+use Sawirricardo\Midtrans\Dto\TransactionDto;
 use Sawirricardo\Midtrans\Midtrans;
 
 it('can create a snap transaction', function () {
     $snapToken = Midtrans::make($_ENV['MIDTRANS_SB_SERVER_KEY'], $_ENV['MIDTRANS_SB_CLIENT_KEY'])
-        ->snap()->create(new \Sawirricardo\Midtrans\Dto\TransactionDto([
+        ->snap()->create(new TransactionDto([
             'transaction_details' => [
                 'order_id' => 'test123',
                 'gross_amount' => 10000,
