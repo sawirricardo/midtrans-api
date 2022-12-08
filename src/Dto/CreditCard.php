f11312a7d731c0418dd2b0bdbf6f264a001479c3
sawirricardo <sawir.ricardo@gmail.com>
2022-12-08 09:01:59 +0700

    make credit card null

--- src/Dto/CreditCard.php
+++ src/Dto/CreditCard.php
@@ -7,11 +7,11 @@ use Spatie\DataTransferObject\DataTransferObject;
 class CreditCard extends DataTransferObject
 {
     public bool $secure;
-    public string $channel;
-    public string $bank;
-    public Installment $installment;
+    public ?string $channel = null;
+    public ?string $bank = null;
+    public ?Installment $installment = null;
 
     /** @var string[] */
-    public array $whitelist_bins;
-    public DynamicDescriptor $dynamic_descriptor;
+    public ?array $whitelist_bins = null;
+    public ?DynamicDescriptor $dynamic_descriptor = null;
 }
